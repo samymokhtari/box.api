fb77967bf0c7c0d19b154500f6b34ae83c6aa807
Samy Mokhtari <samy.mokhtari213@gmail.com>
2023-04-01 00:48:18 +0200

    create sql project, synchronize schema

---
+++ box.database/dbo/Tables/T_Project.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[T_Project] (
+    [id]   INT            IDENTITY (1, 1) NOT NULL,
+    [name] NVARCHAR (255) NOT NULL,
+    [code] NVARCHAR (255) NOT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [CK_Upper_Code] CHECK (upper([code])=([code]) collate Latin1_General_BIN2),
+    CONSTRAINT [AK_Code] UNIQUE NONCLUSTERED ([code] ASC)
+);
+
